7bb6b5944de134f650ff0000b1eec9d60d2b28ed
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2024-07-04 16:28:34 -0500

    Update app.py

--- app.py
+++ app.py
@@ -137,7 +137,7 @@ if canvas_result.image_data is not None and api_key and analyze_button:
                      {
                        "type": "image_url",
                        "image_url": {
-                         "url": f"data:image/jpeg;base64,{base64_image}",
+                         "url": "img.png",
                        },
                      },
                    ],
