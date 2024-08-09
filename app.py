0ee9af6a3c87043ef3f3ab3f5c19de7fe41f9eb1
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2024-08-09 14:04:11 -0500

    Update app.py

--- app.py
+++ app.py
@@ -101,7 +101,7 @@ if profile_ == "Historia":
    Expert= profile_Hist
 if profile_ == "Programación":
    Expert= profile_Prog 
-else
+else :
    Expert= " "
 # Button to trigger the analysis
 analyze_button = st.button("Analiza la imagen", type="secondary")
