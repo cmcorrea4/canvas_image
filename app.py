a34471c0fb696e0d08e3e4e83482bbd2c3e45d14
cmcorrea4 <77401466+cmcorrea4@users.noreply.github.com>
2024-08-14 08:00:34 -0500

    Update app.py

--- app.py
+++ app.py
@@ -35,10 +35,10 @@ with st.sidebar:
 st.subheader("Dibuja el boceto en el panel  y presiona el botón para analizarla")
 
 # Add canvas component
-bg_image = st.file_uploader("Background image:", type=["png", "jpg"])
+bg_image = st.sidebar.file_uploader("Background image:", type=["png", "jpg"])
 # Specify canvas parameters in application
 drawing_mode = "freedraw"
-stroke_width = st.slider('Selecciona el ancho de línea', 1, 30, 5)
+stroke_width = st.sidebar.slider('Selecciona el ancho de línea', 1, 30, 5)
 #stroke_color = '#FFFFFF' # Set background color to white
 #bg_color = '#000000'
 stroke_color = st.color_picker("Elige un Color", "#000000")
